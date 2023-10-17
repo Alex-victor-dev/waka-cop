f5ae6c2dc08fced187add397ed2e89367b75689d
Alex-Victor <alexvictoribeiro@gmail.com>
2023-10-16 22:05:55 -0300

    implementa dominio pauta

---
+++ src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
@@ -0,0 +1,31 @@
+package academy.wakanda.wakacop.pauta.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Pauta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+	private UUID idPauta;
+	private String titulo;
+	private String descricao;
+	private UUID idAssociadoAutor;
+	private LocalDateTime dataCriacao;
+}
